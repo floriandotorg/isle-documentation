xZise/isle

INTERNAL Name all areas

LEGO1/lego/legoomni/src/actors/helicopter.cpp
@@ -95,7 +95,7 @@ void Helicopter::Exit()
 		if (UserActor() && UserActor()->IsA("IslePathActor")) {
 			((IslePathActor*) UserActor())
 				->SpawnPlayer(
-					LegoGameState::e_unk55,
+					LegoGameState::e_unkInFrontJailFacingLeft2,
 					TRUE,
 					IslePathActor::c_spawnBit1 | IslePathActor::c_playMusic | IslePathActor::c_spawnBit3
 				);
@@ -143,7 +143,7 @@ MxLong Helicopter::HandleClick()
 		m_script = *g_isleScript;
 		AnimationManager()->FUN_10064670(NULL);
 		SpawnPlayer(
-			LegoGameState::e_unk41,
+			LegoGameState::e_unkInFrontJailFacingLeft,
 			TRUE,
 			IslePathActor::c_spawnBit1 | IslePathActor::c_playMusic | IslePathActor::c_spawnBit3
 		);
@@ -320,14 +320,14 @@ MxLong Helicopter::HandleEndAnim(LegoEndAnimNotificationParam& p_param)
 			assert(act1State);
 			act1State->m_state = Act1State::e_helicopter;
 			SpawnPlayer(
-				LegoGameState::e_unk42,
+				LegoGameState::e_unkHighAboveJail,
 				TRUE,
 				IslePathActor::c_spawnBit1 | IslePathActor::c_playMusic | IslePathActor::c_spawnBit3
 			);
 		}
 		else {
 			SpawnPlayer(
-				LegoGameState::e_unk49,
+				LegoGameState::e_unkCrash49,
 				TRUE,
 				IslePathActor::c_spawnBit1 | IslePathActor::c_playMusic | IslePathActor::c_spawnBit3
 			);
@@ -361,14 +361,14 @@ MxLong Helicopter::HandleEndAnim(LegoEndAnimNotificationParam& p_param)
 			assert(act1State);
 			act1State->m_state = Act1State::e_none;
 			SpawnPlayer(
-				LegoGameState::e_unk41,
+				LegoGameState::e_unkInFrontJailFacingLeft,
 				TRUE,
 				IslePathActor::c_spawnBit1 | IslePathActor::c_playMusic | IslePathActor::c_spawnBit3
 			);
 		}
 		else {
 			SpawnPlayer(
-				LegoGameState::e_unk48,
+				LegoGameState::e_unkCrash48,
 				TRUE,
 				IslePathActor::c_spawnBit1 | IslePathActor::c_playMusic | IslePathActor::c_spawnBit3
 			);

LEGO1/lego/legoomni/src/actors/islepathactor.cpp
@@ -175,7 +175,7 @@ void IslePathActor::RegisterSpawnLocations()
 		JukeboxScript::c_Quiet_Audio
 	);
 	g_spawnLocations[1] = SpawnLocation(
-		LegoGameState::e_unk23,
+		LegoGameState::e_unkInPizzeria,
 		g_isleScript,
 		0,
 		"edg00_49",
@@ -187,7 +187,7 @@ void IslePathActor::RegisterSpawnLocations()
 		JukeboxScript::c_Quiet_Audio
 	);
 	g_spawnLocations[2] = SpawnLocation(
-		LegoGameState::e_unk24,
+		LegoGameState::e_unkFacingPizzeria,
 		g_isleScript,
 		0,
 		"edg00_191",
@@ -271,7 +271,7 @@ void IslePathActor::RegisterSpawnLocations()
 		JukeboxScript::c_CentralNorthRoad_Music
 	);
 	g_spawnLocations[9] = SpawnLocation(
-		LegoGameState::e_unk21,
+		LegoGameState::e_unkRacetrack,
 		g_isleScript,
 		0,
 		"INT62",
@@ -367,7 +367,7 @@ void IslePathActor::RegisterSpawnLocations()
 		JukeboxScript::c_noneJukebox
 	);
 	g_spawnLocations[17] = SpawnLocation(
-		LegoGameState::e_unk41,
+		LegoGameState::e_unkInFrontJailFacingLeft,
 		g_isleScript,
 		0,
 		"edg02_51",
@@ -415,7 +415,7 @@ void IslePathActor::RegisterSpawnLocations()
 		JukeboxScript::c_noneJukebox
 	);
 	g_spawnLocations[21] = SpawnLocation(
-		LegoGameState::e_unk42,
+		LegoGameState::e_unkHighAboveJail,
 		g_isleScript,
 		0,
 		"inv_05",
@@ -427,7 +427,7 @@ void IslePathActor::RegisterSpawnLocations()
 		JukeboxScript::c_noneJukebox
 	);
 	g_spawnLocations[22] = SpawnLocation(
-		LegoGameState::e_unk48,
+		LegoGameState::e_unkCrash48,
 		g_act3Script,
 		0,
 		"edg02_51",
@@ -439,7 +439,7 @@ void IslePathActor::RegisterSpawnLocations()
 		JukeboxScript::c_noneJukebox
 	);
 	g_spawnLocations[23] = SpawnLocation(
-		LegoGameState::e_unk49,
+		LegoGameState::e_unkCrash49,
 		g_act3Script,
 		0,
 		"inv_05",
@@ -451,7 +451,7 @@ void IslePathActor::RegisterSpawnLocations()
 		JukeboxScript::c_noneJukebox
 	);
 	g_spawnLocations[24] = SpawnLocation(
-		LegoGameState::e_unk50,
+		LegoGameState::e_unkCrash50,
 		g_act2mainScript,
 		0,
 		"EDG02_51",
@@ -463,7 +463,7 @@ void IslePathActor::RegisterSpawnLocations()
 		JukeboxScript::c_noneJukebox
 	);
 	g_spawnLocations[25] = SpawnLocation(
-		LegoGameState::e_unk51,
+		LegoGameState::e_unkInFrontGasStationFacingLeft2,
 		g_isleScript,
 		0,
 		"edg02_32",
@@ -499,7 +499,7 @@ void IslePathActor::RegisterSpawnLocations()
 		JukeboxScript::c_noneJukebox
 	);
 	g_spawnLocations[28] = SpawnLocation(
-		LegoGameState::e_unk55,
+		LegoGameState::e_unkInFrontJailFacingLeft2,
 		g_isleScript,
 		0,
 		"edg02_50",

LEGO1/lego/legoomni/src/worlds/legoact2.cpp
@@ -488,7 +488,7 @@ void LegoAct2::ReadyWorld()
 	m_pepper = FindROI("pepper");
 	IslePathActor* pepper = (IslePathActor*) m_pepper->GetEntity();
 	pepper->SpawnPlayer(
-		LegoGameState::e_unk50,
+		LegoGameState::e_unkCrash50,
 		TRUE,
 		IslePathActor::c_spawnBit1 | IslePathActor::c_playMusic | IslePathActor::c_spawnBit3
 	);

floriandotorg/isle-documentation

LEGO1/lego/legoomni/src/actors/helicopter.cpp
@@ -95,7 +95,7 @@ void Helicopter::Exit()
 		if (UserActor() && UserActor()->IsA("IslePathActor")) {
 			((IslePathActor*) UserActor())
 				->SpawnPlayer(
-					LegoGameState::e_unk55,
+					LegoGameState::e_helicopterExited,
 					TRUE,
 					IslePathActor::c_spawnBit1 | IslePathActor::c_playMusic | IslePathActor::c_spawnBit3
 				);
@@ -143,7 +143,7 @@ MxLong Helicopter::HandleClick()
 		m_script = *g_isleScript;
 		AnimationManager()->FUN_10064670(NULL);
 		SpawnPlayer(
-			LegoGameState::e_unk41,
+			LegoGameState::e_helicopterLanded,
 			TRUE,
 			IslePathActor::c_spawnBit1 | IslePathActor::c_playMusic | IslePathActor::c_spawnBit3
 		);
@@ -320,14 +320,14 @@ MxLong Helicopter::HandleEndAnim(LegoEndAnimNotificationParam& p_param)
 			assert(act1State);
 			act1State->m_state = Act1State::e_helicopter;
 			SpawnPlayer(
-				LegoGameState::e_unk42,
+				LegoGameState::e_helicopterTakenOff,
 				TRUE,
 				IslePathActor::c_spawnBit1 | IslePathActor::c_playMusic | IslePathActor::c_spawnBit3
 			);
 		}
 		else {
 			SpawnPlayer(
-				LegoGameState::e_unk49,
+				LegoGameState::e_unkCrash49,
 				TRUE,
 				IslePathActor::c_spawnBit1 | IslePathActor::c_playMusic | IslePathActor::c_spawnBit3
 			);
@@ -361,14 +361,14 @@ MxLong Helicopter::HandleEndAnim(LegoEndAnimNotificationParam& p_param)
 			assert(act1State);
 			act1State->m_state = Act1State::e_none;
 			SpawnPlayer(
-				LegoGameState::e_unk41,
+				LegoGameState::e_helicopterLanded,
 				TRUE,
 				IslePathActor::c_spawnBit1 | IslePathActor::c_playMusic | IslePathActor::c_spawnBit3
 			);
 		}
 		else {
 			SpawnPlayer(
-				LegoGameState::e_unk48,
+				LegoGameState::e_unkCrash48,
 				TRUE,
 				IslePathActor::c_spawnBit1 | IslePathActor::c_playMusic | IslePathActor::c_spawnBit3
 			);

LEGO1/lego/legoomni/src/actors/islepathactor.cpp
@@ -175,7 +175,7 @@ void IslePathActor::RegisterSpawnLocations()
 		JukeboxScript::c_Quiet_Audio
 	);
 	g_spawnLocations[1] = SpawnLocation(
-		LegoGameState::e_unk23,
+		LegoGameState::e_unkInPizzeria,
 		g_isleScript,
 		0,
 		"edg00_49",
@@ -187,7 +187,7 @@ void IslePathActor::RegisterSpawnLocations()
 		JukeboxScript::c_Quiet_Audio
 	);
 	g_spawnLocations[2] = SpawnLocation(
-		LegoGameState::e_unk24,
+		LegoGameState::e_unkFacingPizzeria,
 		g_isleScript,
 		0,
 		"edg00_191",
@@ -367,7 +367,7 @@ void IslePathActor::RegisterSpawnLocations()
 		JukeboxScript::c_noneJukebox
 	);
 	g_spawnLocations[17] = SpawnLocation(
-		LegoGameState::e_unk41,
+		LegoGameState::e_helicopterLanded,
 		g_isleScript,
 		0,
 		"edg02_51",
@@ -415,7 +415,7 @@ void IslePathActor::RegisterSpawnLocations()
 		JukeboxScript::c_noneJukebox
 	);
 	g_spawnLocations[21] = SpawnLocation(
-		LegoGameState::e_unk42,
+		LegoGameState::e_helicopterTakenOff,
 		g_isleScript,
 		0,
 		"inv_05",
@@ -427,7 +427,7 @@ void IslePathActor::RegisterSpawnLocations()
 		JukeboxScript::c_noneJukebox
 	);
 	g_spawnLocations[22] = SpawnLocation(
-		LegoGameState::e_unk48,
+		LegoGameState::e_unkCrash48,
 		g_act3Script,
 		0,
 		"edg02_51",
@@ -439,7 +439,7 @@ void IslePathActor::RegisterSpawnLocations()
 		JukeboxScript::c_noneJukebox
 	);
 	g_spawnLocations[23] = SpawnLocation(
-		LegoGameState::e_unk49,
+		LegoGameState::e_unkCrash49,
 		g_act3Script,
 		0,
 		"inv_05",
@@ -451,7 +451,7 @@ void IslePathActor::RegisterSpawnLocations()
 		JukeboxScript::c_noneJukebox
 	);
 	g_spawnLocations[24] = SpawnLocation(
-		LegoGameState::e_unk50,
+		LegoGameState::e_pepperSpawnAct2,
 		g_act2mainScript,
 		0,
 		"EDG02_51",
@@ -463,7 +463,7 @@ void IslePathActor::RegisterSpawnLocations()
 		JukeboxScript::c_noneJukebox
 	);
 	g_spawnLocations[25] = SpawnLocation(
-		LegoGameState::e_unk51,
+		LegoGameState::e_unkInFrontGasStationFacingLeft2,
 		g_isleScript,
 		0,
 		"edg02_32",
@@ -499,7 +499,7 @@ void IslePathActor::RegisterSpawnLocations()
 		JukeboxScript::c_noneJukebox
 	);
 	g_spawnLocations[28] = SpawnLocation(
-		LegoGameState::e_unk55,
+		LegoGameState::e_helicopterExited,
 		g_isleScript,
 		0,
 		"edg02_50",

LEGO1/lego/legoomni/src/worlds/legoact2.cpp
@@ -488,7 +488,7 @@ void LegoAct2::ReadyWorld()
 	m_pepper = FindROI("pepper");
 	IslePathActor* pepper = (IslePathActor*) m_pepper->GetEntity();
 	pepper->SpawnPlayer(
-		LegoGameState::e_unk50,
+		LegoGameState::e_pepperSpawnAct2,
 		TRUE,
 		IslePathActor::c_spawnBit1 | IslePathActor::c_playMusic | IslePathActor::c_spawnBit3
 	);